6f4413ed68c9d90e993fe84fd5448d7ab3d22be5
javicoderh <justarapperguy@gmail.com>
2026-04-02 10:44:10 -0300

    vercel deployment bug fixed

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   site: "https://esmasqueunpinguino.cl",
+  output: "server",
+  adapter: vercel(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()]
